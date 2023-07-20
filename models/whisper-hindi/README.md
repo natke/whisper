9ff4113cdac5bb89a0a3136295fec36f22f14d8f
Nat Kershaw <nakersha@microsoft.com>
2023-07-20 09:36:10 -0700

    Updated Olive and README

--- models/whisper-hindi/README.md
+++ models/whisper-hindi/README.md
@@ -1,10 +1,59 @@
 # Config for Hindi transcription
 
-* Clone the latest Olive and install from source
-* Install ORT nightly as per the Olive README
-* Install onnxruntime-extensions 0.8.0
-* Run python prepare_whisper_configs.py --model_name vasista22/whisper-hindi-small --multilingual
-* Run python -m olive.workflows.run --config whisper_cpu_fp32.json --setup
-* Run python -m olive.workflows.run --config whisper_cpu_fp32.json
-* If you are testing with .wav files, edit code/whisper_dataset.py to replace .mp3 with .wav (will need to go * back to .mp3 if you are re-generating the model)
-* Run python test_transcription.py --config whisper_cpu_fp32.json --audio_path "cricketLong-Trimmed (1).wav"
+1. Ceate a new conda environment
+
+```bash
+conda create -n whisper Python=3.9
+conda activate whisper
+```
+
+2. Clone the latest Olive and install from source
+
+```bash`
+git clone git@github.com:microsoft/Olive.git
+cd Olive
+pip install .
+```
+
+3. Install ORT nightly as per the Olive README
+
+```bash
+python -m pip uninstall -y onnxruntime ort-nightly
+python -m pip install ort-nightly --index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple/
+```
+
+4. Install onnxruntime-extensions 0.8.0
+
+```bash
+pip install onnxruntime-extensions
+```
+
+5. Change into the examples/whisper directory
+
+```bash
+cd examples/whisper
+```
+
+6. Prepare the olive configs
+
+```bash
+python prepare_whisper_configs.py --model_name vasista22/whisper-hindi-small --multilingual
+```
+
+7. Setup the model creation
+
+```bash
+python -m olive.workflows.run --config whisper_cpu_fp32.json --setup
+```
+
+8. Generate the model
+
+```bash
+python -m olive.workflows.run --config whisper_cpu_fp32.json
+```
+
+9. Test trancription
+
+```bash
+python test_transcription.py --config whisper_cpu_fp32.json --audio_path "cricketLong-Trimmed (1).wav" --language hi
+```
--- models/whisper-hindi/code/whisper_dataset.py
+++ models/whisper-hindi/code/whisper_dataset.py
@@ -17,9 +17,16 @@ class WhisperDataset:
     N_SAMPLES = CHUNK_LENGTH * SAMPLE_RATE  # 480000 samples in a 30-second chunk
     N_FRAMES = N_SAMPLES // HOP_LENGTH
 
-    def __init__(self, data_dir: str, use_audio_decoder: bool = True):
+    def __init__(
+        self,
+        data_dir: str,
+        use_audio_decoder: bool = True,
+        file_ext: str = ".mp3",
+        language: str = "english",
+        task: str = "transcribe",
+    ):
         data_dir = Path(data_dir)
-        audio_files = list(data_dir.glob("*.wav"))
+        audio_files = list(data_dir.glob(f"*{file_ext}"))
         audio_files.sort(key=lambda x: x.name)
         assert len(audio_files) > 0, f"No audio files found in {data_dir}"
 
@@ -36,22 +43,8 @@ class WhisperDataset:
                 audio_input_name = "audio_pcm"
 
             audio_blob = np.expand_dims(audio_blob, axis=0)  # add a batch_size
-
-
-            model = "openai/whisper-tiny"
-            config = AutoConfig.from_pretrained(model)
-            processor = AutoProcessor.from_pretrained(model)
-
-            # English transcription
-            forced_decoder_ids = processor.get_decoder_prompt_ids(language="hi", task="transcribe")
-            # forced_decoder_ids is of the format [(1, 50259), (2, 50359), (3, 50363)] and needs to be            
-            # of the format [50258, 50259, 50359, 50363] where 50258 is the start token id
-            forced_decoder_ids = [config.decoder_start_token_id] + list(map(lambda token: token[1], forced_decoder_ids))
-
-            print(forced_decoder_ids)
-
             inputs = {
-                 audio_input_name: audio_blob,
+                audio_input_name: audio_blob,
                 "max_length": np.asarray([200], dtype=np.int32),
                 "min_length": np.asarray([0], dtype=np.int32),
                 "num_beams": np.asarray([2], dtype=np.int32),
@@ -60,12 +53,15 @@ class WhisperDataset:
                 "repetition_penalty": np.asarray([1.0], dtype=np.float32),
                 # attention_mask only used when version < 1.16.0
                 "attention_mask": np.zeros((1, self.N_MELS, self.N_FRAMES)).astype(np.int32),
-                # decoder_input_ids only used when version >= 1.16.0 and multilingual is True
-                # auto detect language and task
-                "decoder_input_ids": np.asarray([forced_decoder_ids], dtype=np.int32),
-                # English, transcription
-                # "decoder_input_ids": np.asarray([[50258, 50259, 50359, 50363]], dtype=np.int32),
             }
+            # decoder_input_ids only used when version >= 1.16.0 and multilingual is True
+            model_name = "openai/whisper-tiny"
+            config = AutoConfig.from_pretrained(model_name)
+            processor = AutoProcessor.from_pretrained(model_name)
+            forced_decoder_ids = processor.get_decoder_prompt_ids(language=language, task=task)
+            forced_decoder_ids = [config.decoder_start_token_id] + list(map(lambda token: token[1], forced_decoder_ids))
+            inputs["decoder_input_ids"] = np.asarray([forced_decoder_ids], dtype=np.int32)
+
             self.data.append(inputs)
 
         self.labels = None
